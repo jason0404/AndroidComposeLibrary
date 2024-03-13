bb58ad7b6bcbc22575b8f563ffd8eecb44c435c7
Jason hong <jason.hong@hytechc.com>
2024-03-13 18:13:02 +0800

    2nd commit

--- app/src/main/java/com/example/androidcomposelibrary/MainActivity.kt
+++ app/src/main/java/com/example/androidcomposelibrary/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidcomposelibrary.ui.theme.AndroidComposeLibraryTheme
+import com.example.toaster.ToasterMessage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    ToasterMessage
                 }
             }
         }
